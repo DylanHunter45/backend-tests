d514ec63d005d1f685d9d3c9bd1831c5c1ba1aa3
Dylan Hunter <dylanhunter34@gmail.com>
2025-04-02 15:41:45 -0400

    add authentication and todo routes with basic structure

--- project-2/src/server.js
+++ project-2/src/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
+import authRoutes from "./routes/authRoutes.js";
+import todoRoutes from "./routes/todoRoutes.js";
 
 const app = express();
 const PORT = process.env.PORT || 8000;
@@ -14,6 +16,10 @@ app.use(express.static(path.join(__dirname, "../public")));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
+
+//routes
+app.use("/auth", authRoutes);
+app.use("/todo", todoRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
