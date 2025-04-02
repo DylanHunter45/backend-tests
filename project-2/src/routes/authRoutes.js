ccc023e0ec4f9076fcec5ed131551ab5a92207ae
Dylan Hunter <dylanhunter34@gmail.com>
2025-04-02 16:14:00 -0400

    implement user registration with password hashing, default todo creation, and JWT token generation

--- project-2/src/routes/authRoutes.js
+++ project-2/src/routes/authRoutes.js
@@ -9,9 +9,30 @@ const router = express.Router()
 router.post('/register', async (req, res) => {
     const { username, password } = req.body
 
-    const hashedPassword = bcrypt.hash(password, 10)
-    console.log(username, hashedPassword)
-    res.sendStatus(201)
+    const hashedPassword = bcrypt.hashSync(password, 10)
+
+    try {
+        const insertUser = db.prepare(
+            `INSERT INTO USERS (username, password) VALUES (?, ?)`,
+        )
+        const result = insertUser.run(username, hashedPassword)
+
+        const defaultTodo = `Hello :) Add your first todo!`
+        const insertTodo = db.prepare(
+            `INSERT INTO todos (user_id, task) VALUES (?, ?)`,
+        )
+        insertTodo.run(result.lastInsertRowid, defaultTodo)
+
+        const token = jwt.sign(
+            { id: result.lastInsertRowid },
+            process.env.JWT_SECRET,
+            { expiresIn: '24h' },
+        )
+        res.json({ token })
+    } catch (error) {
+        console.error('Error registering user:', error)
+        return res.status(503)
+    }
 })
 
 // logging in user
